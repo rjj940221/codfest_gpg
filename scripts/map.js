a84fff991c469b5dc7c7d08e17c646f8ab190526
avw1987 <klaus@localhost.localdomain>
2016-10-05 10:12:52 +0200

    Map cleaned up and GPS workaround implemented

--- scripts/map.js
+++ scripts/map.js
@@ -1,18 +1,42 @@
-//checkAvailability(); // start the check
-
 var incidents = [];
-var	gps_on = 0;
+var my_location = new google.maps.LatLng(-26.114779, 27.952908);
+var map_options = {
+	zoom: 13,
+	center: my_location,
+	zoomControl: true,
+	mapTypeControl: false,
+	scaleControl: false,
+	streetViewControl: false,
+	rotateControl: false,
+	fullscreenControl: false
+};
 
 function init_map() {
 	console.log("Opening map");
-	// TODO: prompt the user to activate the gps on failure
-//	checkAvailability();
+	getInfo();
 	navigator.geolocation.getCurrentPosition(on_success, on_error, {
-		timeout: 1
-	});
+		timeout: 1000});
 }
 
-//Fetch incidents from the database
+function draw_map() {
+	var map = new google.maps.Map(document.getElementById("map"), map_options);
+	show_incidents(map);
+	var my_marker = new google.maps.Marker({position: my_location, map: map});
+}
+
+function on_success(position){
+	console.log("GPS activated sucessfully!");
+	var lat = position.coords.latitude;
+	var lng = position.coords.longitude;
+	my_location = new google.maps.LatLng(lat, lng);
+	draw_map();
+}
+
+function on_error(error) {
+	console.log("GPS activation failure!");
+	draw_map();
+}
+
 function getInfo() {
 		var url = "http://owen.exall.za.net/GPG/select_incidents.php";
 		$.getJSON(url, function(result) {
@@ -22,43 +46,12 @@ function getInfo() {
 		});
 }
 
-function on_success(position) {
-	var lat = position.coords.latitude;
-	var lng = position.coords.longitude;
-
-	var my_location = new google.maps.LatLng(lat, lng);
-	var map_options = {
-		zoom: 13,
-		center: my_location,
-		zoomControl: true,
-		mapTypeControl: false,
-		scaleControl: false,
-		streetViewControl: false,
-		rotateControl: false,
-		fullscreenControl: false
-	};
-	console.log(document.getElementById("map"));
-	var map = new google.maps.Map(document.getElementById("map"), map_options);
-	var my_marker = new google.maps.Marker({
-		position: my_location,
-		map: map
-	});
-
-	show_incidents(map);
-}
-
 function show_incidents(map) {
-
-	getInfo();
-	// Origins, anchor positions and coordinates of the marker increase in the X
-	// direction to the right and in the Y direction down.
+	console.log("Initiating incident loop");
 	var image = {
 		url: 'svg/ic_danger_location_black_24px.svg',
-		// This marker is 20 pixels wide by 32 pixels high.
 		size: new google.maps.Size(40, 40),
-		// The origin for this image is (0, 0).
 		origin: new google.maps.Point(0, 0),
-		// The anchor for this image is the base of the flagpole at (0, 32).
 		anchor: new google.maps.Point(14, 40)
 	};
 	console.log(incidents);
@@ -74,12 +67,3 @@ function show_incidents(map) {
 		});
 	});
 }
-
-function on_error(error) {
-
-}
-
-
-//		alert('code: ' + error.code + '\n' +
-//				'message: ' + error.message + '\n');
-//	}
