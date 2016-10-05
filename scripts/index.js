183a0bb2cf355f8cf05a4e10573c888c5256fcdd
= <owen@exall.za.net>
2016-10-05 19:27:11 +0200

    Login Screen now interfaces with the db.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-gpg = angular.module('home', ['ngMaterial', 'ngMessages', 'ngRoute']);
+gpg = angular.module('home', ['ngMaterial', 'ngMessages', 'ngRoute', 'ngStorage']);
 
-gpg.controller('home__', function($scope, $timeout, $mdSidenav, $mdDialog, userId) {
+gpg.controller('home__', function($http, $scope, $timeout, $mdSidenav, $mdDialog, userId, $sessionStorage, $window) {
     document.title = "Home";
 
     $scope.toggleAccount = function() {
@@ -13,13 +13,71 @@ gpg.controller('home__', function($scope, $timeout, $mdSidenav, $mdDialog, userI
     };
 
     $scope.login = function() {
-        if (!$scope.userId) {
+      if (!$scope.userId) {
+            alert("Please enter your login details.");
+        }
+        console.log("User: " + $scope.userId + "\nPassword: " + $scope.userPassword);
+        $scope.isLoading = true;
+        var login = {
+            login: "yes",
+            type: undefined,
+            value: $scope.userId,
+            password: $scope.userPassword
+        };
 
+        if (/^([0-9]{13})$/.test($scope.userId))
+            login.type = "id";
+        else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($scope.userId))
+            login.type = "email";
+        else {
+            $scope.isLoading = false;
+            $scope.DialogMsg = "Invalid ID, Email or Password!";
+            $scope.DialogTitle = "Invalid Login";
+            $mdDialog.show({
+                controller: dialog__,
+                dialogMsg: "Invalid ID, E-mail or Password",
+                dialogTitle: "Invalid Login",
+                templateUrl: "genericDialog.html",
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                fullscreen: false
+            });
+            return ;
         }
 
-        console.log("User: " + $scope.userId + "\nPassword: " + $scope.userPassword);
-        $mdSidenav('account').close();
-        $scope.openMap();
+        console.log(login); //change the url below to the one on the website
+        var request = {
+            method: 'POST',
+            url: '../PHP/authorize.php', //NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            data: login
+        }
+        var user = [];
+        $http(request).then(function successCallback(response, ev) {
+            console.log(response);
+            if (response.data != "false" && response.data != ""){
+                $scope.user = angular.fromJson(response.data);
+                $sessionStorage.user_id = $scope.user.id;
+                $sessionStorage.user_name = $scope.user.first_name;
+                $sessionStorage.rights = $scope.user.admin_rights;
+                $scope.isLoading = false;
+                console.log("Successful Login");
+                $mdSidenav('account').close();
+            } else {
+                console.log("Failed Login");
+                $scope.isLoading = false;
+                $mdDialog.show({
+                    controller: dialog__,
+                    templateUrl: "invalidDialogs/invalid_login.html",
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    fullscreen: false
+                });
+            }
+        }, function errorCallback(response) {
+            console.log("Error");
+            console.log(response);
+        });
     };
 
     $scope.register = function(ev) {
