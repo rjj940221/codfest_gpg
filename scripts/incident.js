842207c9aeb86b426d36928786a922d758afb96f
avw1987 <klaus@localhost.localdomain>
2016-10-06 20:30:54 +0200

    Incidents in progress

--- scripts/incident.js
+++ scripts/incident.js
@@ -1,6 +1,17 @@
 gpg.controller('incident__', function($scope) {
 
-function push_coordinates(position) {
+
+
+    $scope.get_my_loc = function() {
+        console.log("TTTT");
+        navigator.geolocation.getCurrentPosition(push_coordinates, gps_error, {
+            timeout: 5000
+        });
+    };
+
+});
+
+ function push_coordinates(position) {
     console.log("GPS coordinates fetched!");
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
@@ -12,32 +23,33 @@ function push_coordinates(position) {
         lng: lng
     };
 
-    geocoder.geocode({'location': latlng},
-    function(results, status) {
-        if (status === 'OK') {
-            if (results[0]) {
-                map.setZoom(11);
-                var marker = new google.maps.Marker({
-                    position: latlng,
-                    map: map
-                });
-                console.log(results);
-                infowindow.setContent(results[0].formatted_address);
-                infowindow.open(map, marker);
-                //address_array = results[0].formatted_address;
+    geocoder.geocode({
+            'location': latlng
+        },
+        function(results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    map.setZoom(11);
+                    var marker = new google.maps.Marker({
+                        position: latlng,
+                        map: map
+                    });
+                    console.log(results);
+                    infowindow.setContent(results[0].formatted_address);
+                    infowindow.open(map, marker);
+                    //address_array = results[0].formatted_address;
+                } else {
+                    window.alert('No results found');
+                }
             } else {
-                window.alert('No results found');
+                window.alert('Geocoder failed due to: ' + status);
             }
-        } else {
-            window.alert('Geocoder failed due to: ' + status);
-        }
-    });
+        });
 
     console.log(lat);
     console.log(lng);
 }
 
-function gps_error(error) {
+  function gps_error(error) {
     console.log("GPS failure!");
 }
-});
