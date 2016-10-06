c29293d64a0118ff38ad82db83980c612dd905ef
avw1987 <klaus@localhost.localdomain>
2016-10-06 17:30:15 +0200

    Got incidents working again

--- scripts/incident.js
+++ scripts/incident.js
@@ -1,12 +1,43 @@
-function push_coordinates(position){
-	console.log("GPS coordinates fetched!");
-	var lat = position.coords.latitude;
-	var lng = position.coords.longitude;
-	//my_location = new google.maps.LatLng(lat, lng);
-  console.log(lat);
-  console.log(lng);
+gpg.controller('incident__', function($scope) {
+
+function push_coordinates(position) {
+    console.log("GPS coordinates fetched!");
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    var geocoder = new google.maps.Geocoder();
+    var infowindow = new google.maps.InfoWindow();
+    var latlng = {
+        lat: lat,
+        lng: lng
+    };
+
+    geocoder.geocode({'location': latlng},
+    function(results, status) {
+        if (status === 'OK') {
+            if (results[0]) {
+                map.setZoom(11);
+                var marker = new google.maps.Marker({
+                    position: latlng,
+                    map: map
+                });
+                console.log(results);
+                infowindow.setContent(results[0].formatted_address);
+                infowindow.open(map, marker);
+                //address_array = results[0].formatted_address;
+            } else {
+                window.alert('No results found');
+            }
+        } else {
+            window.alert('Geocoder failed due to: ' + status);
+        }
+    });
+
+    console.log(lat);
+    console.log(lng);
 }
 
-function gps_error(error){
-  console.log("GPS failure!");
+function gps_error(error) {
+    console.log("GPS failure!");
 }
+});
