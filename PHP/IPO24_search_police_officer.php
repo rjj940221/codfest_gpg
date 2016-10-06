ef47bbb6233eb042d91f9ae7a0ef645022ec614e
egovfnbcodefest <bintcliffe.a@gmail.com>
2016-10-06 18:39:34 +0200

    new new stuff

---
+++ PHP/IPO24_search_police_officer.php
@@ -0,0 +1,21 @@
+ <?php
+$servername = "lin.arvixe.com";
+$username = "owen_gpg_user";
+$password = "password";
+$request = json_decode(file_get_contents("php://input"));
+if (is_numeric($request)) {
+	$conn = mysqli_connect($servername, $username, $password) or die("failed to connect to datasbase");
+	$query="SELECT * FROM `owen_gpg`.`tb_police_officer` WHERE `id`='".$request."';"; 
+	$result = mysqli_query($con, $query);
+	if ($result)
+	{
+		$station = mysqli_fetch_object($result);
+		echo json_encode($station);
+	}
+	else
+		echo "false";
+	mysqli_close($con);
+} else {
+	    echo "false";
+	}
+?>
---
+++ PHP/IPO26_delete_police_officer.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$servername = "lin.arvixe.com";
+$username = "owen_gpg_user";
+$password = "password";
+$request = json_decode(file_get_contents("php://input"));
+if (is_numeric($request)) 
+{
+	$con = mysqli_connect($servername, $username, $password) or die("failed to connect to datasbase");
+	$sql="DELETE FROM `owen_gpg`.`tb_police_officer` WHERE `id`='".$request."';"; //CHECK THAT * => MAYBE WRONG?
+	if (mysqli_query($con, $sql)) {
+	    echo "success";
+	} else {
+	    echo "fail";
+	}
+	mysqli_close($con);
+} else {
+	echo "false";
+}
+?>
